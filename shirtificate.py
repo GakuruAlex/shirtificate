98784a90a44785d50d1a00d6685428de68fdd80c
= <=>
2025-01-31 11:07:29 +0300

    double

--- shirtificate.py
+++ shirtificate.py
@@ -9,12 +9,12 @@ class PDF(FPDF):
         self.ln(20)
 
     def shirt(self, name: str):
-        pdf = PDF()
-        pdf.add_page(orientation="portrait", format="a4")
-        pdf.set_font(family = "Times", size = 30, style='B')
-        pdf.set_text_color(255,255,255)
-        pdf.cell(0, 213, f"{name} took CS50", align="C")
-        pdf.output("shirtificate.pdf")
+
+        self.add_page(orientation="portrait", format="a4")
+        self.set_font(family = "Times", size = 30, style='B')
+        self.set_text_color(255,255,255)
+        self.cell(0, 213, f"{name} took CS50", align="C")
+        self.output("shirtificate.pdf")
 
 def main():
     pdf = PDF()
