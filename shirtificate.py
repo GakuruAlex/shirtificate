9a5814155c5b6f80bf88ea737b36799802e6be8b
= <=>
2025-01-31 11:05:58 +0300

    using fpdf2

--- shirtificate.py
+++ shirtificate.py
@@ -1,43 +1,24 @@
-from PIL import Image, ImageDraw, ImageFont
-from sys import exit
 from fpdf import FPDF
-class Shirtificate():
-    def __init__(self):
-        pass
-    def open_shirt(self, cs50_shirt: str):
-        try:
-            cs50_shirt = Image.open(cs50_shirt)
-        except FileNotFoundError:
-            exit(f"{cs50_shirt} doesn't exist!")
-        else:
-            return cs50_shirt
-    def print_name_to_shirt(self, cs50_shirt: Image, name: str):
-        font = ImageFont.truetype("./OpenSans-Regular.ttf", 30)
-        shirt_name = ImageDraw.Draw(cs50_shirt)
-        shirt_name.text((150, 200), font=font, text=f"{name} took CS50", fill='white', align='left')
-        cs50_shirt.save('cs50_shirtificate.png')
-        cs50_shirt.close()
+from fpdf import FPDF
 
 class PDF(FPDF):
-    def __init__(self):
-        self.pdf = FPDF(orientation='P', unit='mm', format='A4')
-        self.pdf.add_page()
-        self.pdf.set_font(family='Times', size= 50,style='B')
-    def create_pdf(self, shirt):
-        self.pdf.set_margin(20)
-        self.pdf.cell( text="CS50 Shirtificate", align='C', new_x='LMARGIN', new_y='NEXT', center=True)
-        self.pdf.set_y(50)
-        self.pdf.image(name= shirt, h= 220, w = 170 )
-        self.pdf.output('shirtificate.pdf')
+    def header(self):
+        self.image("./shirtificate.png", 10, 70, 190)
+        self.set_font(family="Times",  size=48, style='B')
+        self.cell(0, 57, "CS50 Shirtificate", align="C")
+        self.ln(20)
 
+    def shirt(self, name: str):
+        pdf = PDF()
+        pdf.add_page(orientation="portrait", format="a4")
+        pdf.set_font(family = "Times", size = 30, style='B')
+        pdf.set_text_color(255,255,255)
+        pdf.cell(0, 213, f"{name} took CS50", align="C")
+        pdf.output("shirtificate.pdf")
 
 def main():
-    shirtificate = Shirtificate()
-    shirt = shirtificate.open_shirt("shirtificate.png")
-    shirtificate.print_name_to_shirt(shirt, input("Name: "))
     pdf = PDF()
-    pdf.create_pdf(shirt= './cs50_shirtificate.png')
+    pdf.shirt(input("Name: "))
 
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
